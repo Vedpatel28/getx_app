9ce851eb70625505bc7b55e6e6abaa6da2712aa8
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-08-18 18:53:43 +0530

    Update cart_controller.dart
    | VP | 18/08/23 | 06:55 | cart System Added...

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,8 +1,31 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types, unrelated_type_equality_checks
 
 import 'package:get/get.dart';
 import 'package:getx_app/model/api_model.dart';
 
 class cartController extends GetxController {
   RxList<productModel> cartItems = <productModel>[].obs;
+  RxInt totalPay = 0.obs;
+
+  addProduct({required productModel product}) {
+    cartItems.add(product);
+  }
+
+  removeProduct({required productModel product}) {
+    cartItems.remove(product);
+  }
+
+  increaseItems({required productModel product}) {
+    product.totalItems.value++;
+    update();
+  }
+
+  decreaseItems({required productModel product}) {
+    if (product.totalItems == 0) {
+      removeProduct(product: product);
+    } else {
+      product.totalItems.value--;
+    }
+    update();
+  }
 }
