d315e7e68c71d1eaeb020f71efa27aea53ec0943
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-08-18 18:55:01 +0530

    Update ecom_cart.dart
    | VP | 18/08/23 | 06:55 | cart System Added...

--- lib/views/screens/ecom_cart.dart
+++ lib/views/screens/ecom_cart.dart
@@ -1,13 +1,64 @@
+// ignore_for_file: must_be_immutable, camel_case_types
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_app/controller/cart_controller.dart';
+import 'package:getx_app/model/api_model.dart';
 
 class Ecom_Cart extends StatelessWidget {
-  const Ecom_Cart({super.key});
+  Ecom_Cart({super.key});
+
+  cartController cartcontroller = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("E Com Cart"),
+        title: const Text("E Commerce Cart"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 600,
+              child: ListView.builder(
+                itemCount: cartcontroller.cartItems.length,
+                itemBuilder: (context, index) {
+                  productModel product = cartcontroller.cartItems[index];
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        foregroundImage: NetworkImage(product.thumbnail),
+                      ),
+                      title: Text(product.title),
+                      subtitle: Container(
+                        width: 60,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                cartcontroller.decreaseItems(product: product);
+                              },
+                              icon: const Icon(Icons.remove_circle_outline_sharp),
+                            ),
+                            Obx(() => Text("${product.totalItems}"),),
+                            IconButton(
+                              onPressed: () {
+                                cartcontroller.increaseItems(product: product);
+                              },
+                              icon: const Icon(Icons.add_circle_outline_sharp),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
