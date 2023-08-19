01fa1a611de7f1cf51793bbf2f7ede3eeba42442
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-08-19 11:49:44 +0530

    Update ecom_cart.dart
    | VP | 19/08/23 | 11:48 |  Completed . . .

--- lib/views/screens/ecom_cart.dart
+++ lib/views/screens/ecom_cart.dart
@@ -19,44 +19,60 @@ class Ecom_Cart extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            SizedBox(
-              height: 600,
-              child: ListView.builder(
-                itemCount: cartcontroller.cartItems.length,
-                itemBuilder: (context, index) {
-                  productModel product = cartcontroller.cartItems[index];
-                  return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        foregroundImage: NetworkImage(product.thumbnail),
-                      ),
-                      title: Text(product.title),
-                      subtitle: Container(
-                        width: 60,
-                        child: Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                cartcontroller.decreaseItems(product: product);
-                              },
-                              icon: const Icon(Icons.remove_circle_outline_sharp),
-                            ),
-                            Obx(() => Text("${product.totalItems}"),),
-                            IconButton(
-                              onPressed: () {
-                                cartcontroller.increaseItems(product: product);
-                              },
-                              icon: const Icon(Icons.add_circle_outline_sharp),
-                            ),
-                          ],
+            Obx(
+              () => Container(
+                height: 600,
+                child: ListView.builder(
+                  itemCount: cartcontroller.cartItems.length,
+                  itemBuilder: (context, index) {
+                    productModel product = cartcontroller.cartItems[index];
+                    return Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          foregroundImage: NetworkImage(product.thumbnail),
+                        ),
+                        title: Text(product.title),
+                        subtitle: Container(
+                          width: 60,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  cartcontroller.decreaseItems(
+                                    product: product,
+                                  );
+                                },
+                                icon: const Icon(
+                                    Icons.remove_circle_outline_sharp),
+                              ),
+                              Obx(
+                                () => Text("${product.totalItems}"),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  cartcontroller.increaseItems(
+                                      product: product);
+                                },
+                                icon:
+                                    const Icon(Icons.add_circle_outline_sharp),
+                              ),
+                            ],
+                          ),
+                        ),
+                        trailing: Obx(
+                          () => Text(
+                              "\$ ${cartcontroller.totalAmount(product: product)}"),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
+            const Spacer(),
+            Text("Total Amount \$ ${cartcontroller.pay}"),
           ],
         ),
       ),
